eea7eb67d3e6f0e5b59b7245a2b3cce04d8afcaa
Alexandre Leuck <aleuck@inf.ufrgs.br>
2017-04-12 09:38:01 -0300

    main inicial

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include "hash.h"
+#include "lex.yy.h"
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    int token;
+    if (argc < 2) {
+        printf("");
+        exit(1);
+    }
+
+    yyin = fopen(argv[1], "r");
+    if (!yyin) {
+        printf("Não foi possível abrir o arquivo.");
+    }
     return 0;
 }
