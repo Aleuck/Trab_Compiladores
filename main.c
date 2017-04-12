65fcde5ca3803637b943b950cfa61ab3792c0401
Alexandre Leuck <aleuck@inf.ufrgs.br>
2017-04-12 12:45:10 -0300

    completando a main

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include "lex.yy.h"
 #include <stdio.h>
 
+extern int getLineNumber(void);
+extern int running;
+
 int main(int argc, char *argv[]) {
     int token;
     if (argc < 2) {
@@ -12,6 +15,10 @@ int main(int argc, char *argv[]) {
     yyin = fopen(argv[1], "r");
     if (!yyin) {
         printf("Não foi possível abrir o arquivo.");
+    }
+    while (running) {
+        token = yylex();
+        printf("(%d):\t%d\n", getLineNumber(), token);
     }
     return 0;
 }
