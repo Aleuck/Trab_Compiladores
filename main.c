75ef46729cee81c20e56021d4f17f527974ceaea
Rubens Ideron <rubens.ror@hotmail.com>
2017-04-13 14:58:01 -0300

    pre-correcoes

--- main.c
+++ main.c
@@ -3,20 +3,20 @@
 #include <stdio.h>
 
 extern int getLineNumber(void);
-extern int running;
+extern int isRunning(void);
 
 int main(int argc, char *argv[]) {
     int token;
     if (argc < 2) {
-        printf("");
+        printf("Por favor especifique o nome do arquivo de entrada.\n");
         exit(1);
     }
 
     yyin = fopen(argv[1], "r");
     if (!yyin) {
-        printf("Não foi possível abrir o arquivo.");
+        printf("Não foi possível abrir o arquivo.\n");
     }
-    while (running) {
+    while (isRunning()) {
         token = yylex();
         printf("(%d):\t%d\n", getLineNumber(), token);
     }
