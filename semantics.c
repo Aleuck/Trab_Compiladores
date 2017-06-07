95f34fe6e5d70dc1f062e0e6cfa62ba07f5cbe9a
elrubo <rubens.ror@hotmail.com>
2017-06-07 18:58:48 -0300

    reunião

--- semantics.c
+++ semantics.c
@@ -31,6 +31,7 @@ void semanticSetDeclarations(AST *ast_node){
         if(!ast_node->symbol)
         {
             fprintf(stderr, "árvore bugada");
+            exit(666);
         }
         if(ast_node->symbol->token_type != TK_IDENTIFIER)
         {
@@ -108,31 +109,179 @@ void assertProperUse(AST *ast_node){
                     semanticError = 1;
                 }
 
-                if(!checkExpValidity(ast_node->son[0]))
+                if(!checkExpValidityAssign(ast_node->son[0]))
                 {
                   fprintf(stderr, "Semantic Error: not a valid expression on \"%s\" var assign.\n", ast_node->symbol->text);
                   semanticError = 1;
                 }
-                else if(!compatibleAssignTypes(ast_node->symbol->decl->data_type, ast_node->son[0]->data_type))
+//                else if(!compatibleAssignTypes(ast_node->symbol->decl->data_type, ast_node->son[0]->data_type))
+//                {
+//                  fprintf(stderr, "Semantic Error: incompatible types on \"%s\" var assign.\n", ast_node->symbol->text);
+//                  semanticError = 1;
+//                }
+                break;
+
+        case AST_VECTOR_ASSIGN:
+                if(ast_node->symbol->token_type != SYMBOL_VECTOR)
                 {
-                  fprintf(stderr, "Semantic Error: incompatible types on \"%s\" var assign.\n", ast_node->symbol->text);
+                    fprintf(stderr, "Semantic Error: \"%s\" not a valid symbol - vector assign.\n", ast_node->symbol->text);
+                    semanticError = 1;
+                }
+                
+                if(!checkIntExp(ast_node->son[0]))  //index should be int
+                {
+                    fprintf(stderr, "Semantic Error: not a valid index on \"%s\" vector assign.\n", ast_node->symbol->text);
+                    semanticError = 1;
+                }
+
+                if(!checkExpValidityAssign(ast_node->son[1]))
+                {
+                  fprintf(stderr, "Semantic Error: not a valid expression on \"%s\" vector assign.\n", ast_node->symbol->text);
                   semanticError = 1;
                 }
+//                else if(!compatibleAssignTypes(ast_node->symbol->decl->data_type, ast_node->son[0]->data_type))
+//                {
+//                  fprintf(stderr, "Semantic Error: incompatible types on \"%s\" var assign.\n", ast_node->symbol->text);
+//                  semanticError = 1;
+//                }
                 break;
 
         case AST_SUM:
-                ast_node->data_type = assertExpTypeAddSub(ast_node->son[0]->data_type, ast_node->son[1]->data_type);
-                break;
-
         case AST_SUBT:
-                ast_node->data_type = assertExpTypeAddSub(ast_node->son[0]->data_type, ast_node->son[1]->data_type);
-                break;
         case AST_LONE_MINUS:
                 ast_node->data_type = assertExpTypeAddSub(ast_node->son[0]->data_type, ast_node->son[1]->data_type);
                 break;
-
+                
+        case AST_GREATER :
+        case AST_LESS    : 
+        case AST_LE      :  
+        case AST_GE      :  
+        case AST_EQ      :  
+        case AST_NE      :
+                ast_node->data_type = assertExpTypeBool1(ast_node->son[0]->data_type, ast_node->son[1]->data_type);
+                break;
+        case AST_AND     :  
+        case AST_OR      :  
+                ast_node->data_type = assertExpTypeBool2(ast_node->son[0]->data_type, ast_node->son[1]->data_type);
+                break;
+        case AST_NOT     :
+                ast_node->data_type = assertExpTypeBool2(ast_node->son[0]->data_type, DATATYPE_bool);
+                break;
+        
+        case AST_VECTOR:
+                if(ast_node->symbol->token_type != SYMBOL_VECTOR)
+                {
+                    fprintf(stderr, "Semantic Error: \"%s\" not a valid symbol - vector on exp.\n", ast_node->symbol->text);
+                    semanticError = 1;
+                }
+                else
+                {
+                    ast_node->data_type = ast_node->symbol->decl->data_type;        //inherits data_type from declaration
+                }
+                
+                if(!checkIntExp(ast_node->son[0]))  //index should be int
+                {
+                    fprintf(stderr, "Semantic Error: not a valid index on \"%s\" vector on exp.\n", ast_node->symbol->text);
+                    semanticError = 1;
+                }
+                break;
+                
+        case AST_IDENTIFIER:
+                if(ast_node->symbol->token_type != SYMBOL_LOCAL_VAR && ast_node->symbol->token_type != SYMBOL_VAR)
+                {
+                    fprintf(stderr, "Semantic Error: \"%s\" not a valid symbol - var on exp.\n", ast_node->symbol->text);
+                    semanticError = 1;
+                }
+                else
+                {
+                    ast_node->data_type = ast_node->symbol->decl->data_type;        //inherits data_type from declaration
+                }
+                break;
+        
+        case AST_LIT_INTEGER:
+        case AST_LIT_REAL:
+        case AST_LIT_CHAR:
+                break;
     }
 }
+
+int checkIntExp(AST *node){ //insures exp type is integer
+    if(node)
+        if (node->data_type == DATATYPE_BYTE || node->data_type == DATATYPE_SHORT || node->data_type == DATATYPE_LONG)
+            return 1;
+    
+    return 0;
+}
+
+int checkExpValidityAssign(AST *node){
+  if(node && node->son[0]->data_type != 0 && node->son[0]->data_type != DATATYPE_bool)
+      return 1;
+  return 0;
+}
+
+int checkExpValidityBool(AST *node){
+  if(node && node->son[0]->data_type == DATATYPE_bool)
+      return 1;
+  return 0;
+}
+
+int assertExpTypeBool1(int type1, int type2){       //used for < > <= >= == !=
+    if(type1 == DATATYPE_bool || type2 == DATATYPE_bool || type1 == 0 || type2 == 0)
+        return 0;                                           // datatype error
+
+    return DATATYPE_bool;
+}
+
+int assertExpTypeBool2(int type1, int type2){       //used for ! && ||
+    if(type1 != DATATYPE_bool || type2 != DATATYPE_bool || type1 == 0 || type2 == 0)
+        return 0;                                           // datatype error
+
+    return DATATYPE_bool;
+}
+
+int assertExpTypeAddSub(int type1, int type2){
+
+    if(type1 == DATATYPE_bool || type2 == DATATYPE_bool)
+        return 0;                                           // datatype error
+
+    if(type1 == DATATYPE_BYTE && type2 == DATATYPE_BYTE)
+        return DATATYPE_BYTE;
+    if(type1 == DATATYPE_FLOAT && type2 == DATATYPE_FLOAT)
+        return DATATYPE_FLOAT;
+    if(type1 == DATATYPE_DOUBLE && type2 == DATATYPE_DOUBLE)
+        return DATATYPE_DOUBLE;
+    if(type1 == DATATYPE_SHORT && type2 == DATATYPE_SHORT)
+        return DATATYPE_SHORT;
+    if(type1 == DATATYPE_LONG && type2 == DATATYPE_LONG)
+        return DATATYPE_LONG;
+
+    if((type1 == DATATYPE_SHORT && type2 == DATATYPE_LONG) || (type2 == DATATYPE_SHORT && type1 == DATATYPE_LONG))
+        return DATATYPE_LONG;
+    if((type1 == DATATYPE_SHORT && type2 == DATATYPE_BYTE) || (type2 == DATATYPE_SHORT && type1 == DATATYPE_BYTE))
+        return DATATYPE_SHORT;
+    if((type1 == DATATYPE_SHORT && type2 == DATATYPE_FLOAT) || (type2 == DATATYPE_SHORT && type1 == DATATYPE_FLOAT))
+        return DATATYPE_FLOAT;
+    if((type1 == DATATYPE_SHORT && type2 == DATATYPE_DOUBLE) || (type2 == DATATYPE_SHORT && type1 == DATATYPE_DOUBLE))
+        return DATATYPE_DOUBLE;
+
+    if((type1 == DATATYPE_BYTE && type2 == DATATYPE_LONG) || (type2 == DATATYPE_BYTE && type1 == DATATYPE_LONG))
+        return DATATYPE_LONG;
+    if((type1 == DATATYPE_BYTE && type2 == DATATYPE_FLOAT) || (type2 == DATATYPE_BYTE && type1 == DATATYPE_FLOAT))
+        return DATATYPE_FLOAT;
+    if((type1 == DATATYPE_BYTE && type2 == DATATYPE_DOUBLE) || (type2 == DATATYPE_BYTE && type1 == DATATYPE_DOUBLE))
+        return DATATYPE_DOUBLE;
+
+    if((type1 == DATATYPE_LONG && type2 == DATATYPE_FLOAT) || (type2 == DATATYPE_LONG && type1 == DATATYPE_FLOAT))
+        return DATATYPE_FLOAT;
+    if((type1 == DATATYPE_LONG && type2 == DATATYPE_DOUBLE) || (type2 == DATATYPE_LONG && type1 == DATATYPE_DOUBLE))
+        return DATATYPE_DOUBLE;
+
+    if((type1 == DATATYPE_DOUBLE && type2 == DATATYPE_FLOAT) || (type2 == DATATYPE_DOUBLE && type1 == DATATYPE_FLOAT))
+        return DATATYPE_DOUBLE;
+
+}
+
+/* especificação pede pra NÃO fazer isso
 
 int compatibleAssignTypes(int type1, int type2){
 
@@ -186,50 +335,4 @@ int compatibleAssignTypes(int type1, int type2){
 
     return 0;
 }
-
-int assertExpTypeAddSub(int type1, int type2){
-
-    if(type1 == DATATYPE_bool || type2 == DATATYPE_bool)
-        return 0;
-
-    if(type1 == DATATYPE_BYTE && type2 == DATATYPE_BYTE)
-        return DATATYPE_BYTE;
-    if(type1 == DATATYPE_FLOAT && type2 == DATATYPE_FLOAT)
-        return DATATYPE_FLOAT;
-    if(type1 == DATATYPE_DOUBLE && type2 == DATATYPE_DOUBLE)
-        return DATATYPE_DOUBLE;
-    if(type1 == DATATYPE_SHORT && type2 == DATATYPE_SHORT)
-        return DATATYPE_SHORT;
-    if(type1 == DATATYPE_LONG && type2 == DATATYPE_LONG)
-        return DATATYPE_LONG;
-
-    if((type1 == DATATYPE_SHORT && type2 == DATATYPE_LONG) || (type2 == DATATYPE_SHORT && type1 == DATATYPE_LONG))
-        return DATATYPE_LONG;
-    if((type1 == DATATYPE_SHORT && type2 == DATATYPE_BYTE) || (type2 == DATATYPE_SHORT && type1 == DATATYPE_BYTE))
-        return DATATYPE_SHORT;
-    if((type1 == DATATYPE_SHORT && type2 == DATATYPE_FLOAT) || (type2 == DATATYPE_SHORT && type1 == DATATYPE_FLOAT))
-        return DATATYPE_FLOAT;
-    if((type1 == DATATYPE_SHORT && type2 == DATATYPE_DOUBLE) || (type2 == DATATYPE_SHORT && type1 == DATATYPE_DOUBLE))
-        return DATATYPE_DOUBLE;
-
-    if((type1 == DATATYPE_BYTE && type2 == DATATYPE_LONG) || (type2 == DATATYPE_BYTE && type1 == DATATYPE_LONG))
-        return DATATYPE_LONG;
-    if((type1 == DATATYPE_BYTE && type2 == DATATYPE_FLOAT) || (type2 == DATATYPE_BYTE && type1 == DATATYPE_FLOAT))
-        return DATATYPE_FLOAT;
-    if((type1 == DATATYPE_BYTE && type2 == DATATYPE_DOUBLE) || (type2 == DATATYPE_BYTE && type1 == DATATYPE_DOUBLE))
-        return DATATYPE_DOUBLE;
-
-    if((type1 == DATATYPE_LONG && type2 == DATATYPE_FLOAT) || (type2 == DATATYPE_LONG && type1 == DATATYPE_FLOAT))
-        return DATATYPE_FLOAT;
-    if((type1 == DATATYPE_LONG && type2 == DATATYPE_DOUBLE) || (type2 == DATATYPE_LONG && type1 == DATATYPE_DOUBLE))
-        return DATATYPE_DOUBLE;
-
-    if((type1 == DATATYPE_DOUBLE && type2 == DATATYPE_FLOAT) || (type2 == DATATYPE_DOUBLE && type1 == DATATYPE_FLOAT))
-        return DATATYPE_DOUBLE;
-
-}
-
-int checkExpValidity(AST *node){
-  if(node && node->son[0]->data_type)
-      return 1;
-}
+*/
